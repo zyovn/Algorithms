0d3244608a37778468f1b99b4c6f138c587e58fa
Joo Jeong bin <166782961+zyovn@users.noreply.github.com>
2024-09-25 02:01:44 +0900

    Update 수 정렬하기 2.py

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -4,7 +4,7 @@ n = int(input())
 list = []
 
 for i in range(n):
-    list.append(int(sys.stdin.readline()))
+    list.append(int(sys.stdin.readline())) #시간 초과로 인해서 sys.stdin.readline() 사용
 list.sort()
 
 for i in list:
