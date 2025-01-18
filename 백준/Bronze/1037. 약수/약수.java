4fd2d6933c9a8cec82e09ab7ebfcc7cef0c61ee3
Joo Jeong bin <166782961+zyovn@users.noreply.github.com>
2025-01-18 13:41:38 +0900

    [Bronze I] Title: 약수, Time: 108 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static List<Integer> arr = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(arr);
+        System.out.println(arr.get(0) * arr.get(arr.size() - 1));
+    }
+}
