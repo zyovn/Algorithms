09ad8ad66ba343c0541394caa353d4213b9a27af
Joo Jeong bin <166782961+zyovn@users.noreply.github.com>
2025-01-23 19:05:53 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18088 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        dp[1] = 0;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1; // 항상 가능 -> 먼저 시도
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+        System.out.println(dp[N]);
+    }
+}
