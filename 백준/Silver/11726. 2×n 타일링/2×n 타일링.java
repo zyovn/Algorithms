3eaf0df4627478ba574f8ceba6123d33fca2a705
Joo Jeong bin <166782961+zyovn@users.noreply.github.com>
2025-01-30 19:01:48 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp = new int[1001];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
