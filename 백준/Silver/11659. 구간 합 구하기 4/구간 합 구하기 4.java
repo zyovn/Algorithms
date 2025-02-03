e8bda8d803ca0b494515db7d32cac9c976697974
Joo Jeong bin <166782961+zyovn@users.noreply.github.com>
2025-02-03 20:11:50 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1112 ms, Memory: 59432 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr, prefixSum;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1];
+        prefixSum = new int[N + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            prefixSum[i] = prefixSum[i - 1] + arr[i]; // 입력 시, 바로 누적합
+        }
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            System.out.println(prefixSum[m] - prefixSum[n - 1]);
+        }
+    }
+}
