f5b7bf944fdd012f37a3311d2fbcd4a98eb9c738
Joo Jeong bin <166782961+zyovn@users.noreply.github.com>
2025-02-06 04:17:54 +0900

    [Silver I] Title: 어두운 건 무서워, Time: 640 ms, Memory: 86952 KB -BaekjoonHub

---
+++ 백준/Silver/16507. 어두운 건 무서워/어두운 건 무서워.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int R, C, Q;
+    static int[][] arr, prefixSum;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+        arr = new int[R + 1][C + 1];
+        prefixSum = new int[R + 1][C + 1];
+
+        for (int i = 1; i <= R; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= C; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                prefixSum[i][j] = arr[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
+            }
+        }
+        for (int i = 1; i <= Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r1 = Integer.parseInt(st.nextToken());
+            int c1 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+            System.out.println((prefixSum[r2][c2] - prefixSum[r1 - 1][c2] - prefixSum[r2][c1 - 1] + prefixSum[r1 - 1][c1 - 1]) / ((r2 - r1 + 1) * (c2 - c1 + 1)));
+        }
+    }
+}
