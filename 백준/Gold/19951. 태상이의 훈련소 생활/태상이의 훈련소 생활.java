68e76d8a45d6dbe9996e4f4d2c53a6474e3061b2
Joo Jeong bin <166782961+zyovn@users.noreply.github.com>
2025-02-07 20:15:44 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 516 ms, Memory: 62856 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -1,49 +1,38 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int N; // 연병장의 길이
-    static int M; // 조교의 수
-    static int[] arr; // 연병장 배열
-    static int[] prefixSum; // 작업 - 누적합 배열
+    static int N, M;
+    static int[] arr, prefixSum;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        // 연병장의 길이 = N, 조교의 수 = M
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1];
+        prefixSum = new int[N + 2];
 
-        arr = new int[N+1];
-        prefixSum = new int[N+1];
-
-        // 연병장 입력 받기
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < N+1; i++) {
+        for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        // 작업 입력 받기
-        for (int i = 0; i < M; i++) {
+
+        for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
 
-            prefixSum[start] += weight;
-            if (end + 1 <= N) {
-                prefixSum[end + 1] -= weight;
-            }
-
+            prefixSum[a] += k;
+            prefixSum[b + 1] -= k;
         }
-        // 누적합
-        StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i <= N; i++) {
-            prefixSum[i] += prefixSum[i-1];
-            sb.append((arr[i] + prefixSum[i])).append(" ");
+            prefixSum[i] += prefixSum[i - 1];
+            sb.append(arr[i] + prefixSum[i]).append(" ");
         }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
