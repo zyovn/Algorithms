6986e8f844433cb8bdbf9467a8769c543a5013ce
Joo Jeong bin <166782961+zyovn@users.noreply.github.com>
2025-03-27 20:47:57 +0900

    [Silver V] Title: 날짜 계산, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int E, S, M, result;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        E = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        result = 1;
+
+        while (true) {
+            if ((result - E) % 15 == 0 && (result - S) % 28 == 0 && (result - M) % 19 == 0) break;
+            result++;
+        }
+        System.out.println(result);
+    }
+}
