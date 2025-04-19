130dd04733ae16541b272f5fac938259e8343e26
Joo Jeong bin <166782961+zyovn@users.noreply.github.com>
2025-04-19 14:13:07 +0900

    [Silver V] Title: 셀프 넘버, Time: 100 ms, Memory: 13920 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -9,22 +9,18 @@ public class Main {
         // 셀프 넘버 X
         for (int i = 1; i < 10001; i++) {
             int tmp = d(i);
-            if (tmp < 10001) {
-                check[tmp] = true;
-            }
+            if (tmp < 10001) check[tmp] = true;
         }
         // 셀프 넘버
         for (int i = 1; i < 10001; i++) {
-            if (!check[i]) {
-                sb.append(i).append("\n");
-            }
+            if (!check[i]) sb.append(i).append("\n");
         }
         System.out.println(sb);
     }
     private static int d(int num) {
         int sum = num;
 
-        while (num != 0) {
+        while (num !=0) {
             sum = sum + (num % 10);
             num /= 10;
         }
