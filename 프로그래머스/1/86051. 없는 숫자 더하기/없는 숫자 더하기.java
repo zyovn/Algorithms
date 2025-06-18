494d9dc244b315dea1a5350c20a291bc66e0c51f
Jeongbin Joo <166782961+zyovn@users.noreply.github.com>
2025-06-18 18:27:40 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 0;
+        
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return 45 - sum;
+    }
+}
