e248f187571e6f18e658a8266cdbbbb3ecc9a221
Jeongbin Joo <166782961+zyovn@users.noreply.github.com>
2025-06-21 02:56:32 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 624 ms, Memory: 52476 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[][] coordinate;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        coordinate = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            coordinate[i][0] = Integer.parseInt(st.nextToken());
+            coordinate[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(coordinate, (o1, o2) ->
+                o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1]
+        );
+
+        for (int i = 0; i < N; i++) {
+            sb.append(coordinate[i][0]).append(" ").append(coordinate[i][1]).append("\n");
+        }
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
