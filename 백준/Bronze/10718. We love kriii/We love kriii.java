44931e8feb3c731c91347fbede68764578989c52
Jeongbin Joo <166782961+zyovn@users.noreply.github.com>
2025-06-20 13:34:58 +0900

    [Bronze V] Title: We love kriii, Time: 92 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 2; i++) {
+            sb.append("강한친구").append(" ").append("대한육군").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
