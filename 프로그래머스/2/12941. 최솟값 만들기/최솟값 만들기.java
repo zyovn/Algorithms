155001294613796f94cb472cf4afe9ea20552ddd
Jeongbin Joo <166782961+zyovn@users.noreply.github.com>
2025-07-12 02:18:30 +0900

    [level 2] Title: 최솟값 만들기, Time: 4.27 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
 class Solution {
-    static int result;
     public int solution(int []A, int []B) {
+        int answer = 0;
+        
         Arrays.sort(A);
         Arrays.sort(B);
         
         for (int i = 0; i < A.length; i++) {
-            result += A[i] * B[B.length - 1 - i];
+            answer += A[i] * B[B.length - 1 - i];
         }
-        return result;
+        return answer;
     }
 }
