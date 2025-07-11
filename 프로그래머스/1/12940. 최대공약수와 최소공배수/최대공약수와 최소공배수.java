c6a930c7b392fd0028e362ad8aef194abd3d3cb6
Jeongbin Joo <166782961+zyovn@users.noreply.github.com>
2025-07-12 02:44:53 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        answer[0] = gcd(n, m);
+        answer[1] = lcm(n, m);
+        
+        return answer;
+    }
+    
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+        return a;
+    }
+    
+    private int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
