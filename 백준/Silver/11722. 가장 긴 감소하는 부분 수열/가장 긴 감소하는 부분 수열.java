7735229d2182bd10d2d3244ef4aad6dd66ed0692
Jeongbin Joo <166782961+zyovn@users.noreply.github.com>
2025-11-25 15:54:51 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 128 ms, Memory: 14792 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] A, dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        A = new int[N + 1];
+        dp = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        int result = 1;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                if (A[i] < A[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            result = Math.max(result, dp[i]);
+        }
+        sb.append(result);
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
