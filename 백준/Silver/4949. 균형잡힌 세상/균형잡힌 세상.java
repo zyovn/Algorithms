00d83bfe28d856c4cefd587e59e9b62292bd8f24
Jeongbin Joo <166782961+zyovn@users.noreply.github.com>
2025-12-16 02:14:49 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 196 ms, Memory: 19368 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -17,7 +17,6 @@ public class Main {
 
             for (int i = 0; i < str.length(); i++) {
                 char ch = str.charAt(i);
-
                 if (ch == '(' || ch == '[') stack.push(ch);
                 else if (ch == ')') {
                     if (stack.isEmpty() || stack.peek() != '(') {
