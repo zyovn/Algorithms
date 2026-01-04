9d1b1056ea2fedf432766d3c879fb514506f0185
Jeongbin Joo <yui2507@naver.com>
2026-01-04 14:52:42 +0900

    [Silver III] Title: N과 M (1), Time: 228 ms, Memory: 22896 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,38 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
+    static int N, M;
     static int[] arr;
     static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
         arr = new int[M];
         visited = new boolean[N];
 
         BackTracking(0);
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
-    public static void BackTracking(int depth) {
+
+    private static void BackTracking(int depth) {
         if (depth == M) {
             for (int i = 0; i < M; i++) {
-                System.out.println(arr[i]);
+                sb.append(arr[i]).append(" ");
             }
+            sb.append("\n");
             return;
         }
         for (int i = 0; i < N; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                arr[depth] = i + 1; // 선택한 숫자 저장
-                BackTracking(depth + 1); // 재귀 -> 다음 깊이로 이동하여 숫자 선택
-                visited[i] = false; // 원상 복구 -> 다른 조합 탐색
+                arr[depth] = i + 1;
+                BackTracking(depth + 1);
+                visited[i] = false;
             }
         }
     }
