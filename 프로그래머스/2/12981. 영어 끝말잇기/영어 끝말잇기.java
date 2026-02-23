d5c656598311cc5a941f2bd0b495468ff0733ee0
Jeongbin Joo <yui2507@naver.com>
2026-02-23 11:34:53 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -9,13 +9,11 @@ class Solution {
 
         for (int i = 1; i < words.length; i++) {
             if (set.contains(words[i]) || 
-                words[i].charAt(0) != words[i - 1].charAt(words[i - 1].length() - 1)
-               ) {
+                words[i].charAt(0) != words[i - 1].charAt(words[i - 1].length() - 1)) {
                 answer[0] = (i % n) + 1;
                 answer[1] = (i / n) + 1;
                 break;
-            }
-            else set.add(words[i]);
+            } else set.add(words[i]);
         }
         return answer;
     }
