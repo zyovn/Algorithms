6adaba630b644ad18aa2491eae2e0c0be238ae72
Jeongbin Joo <yui2507@naver.com>
2026-03-12 12:14:40 +0900

    [Gold V] Title: 적록색약, Time: 120 ms, Memory: 14696 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,78 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
 
 public class Main {
     static int N;
-    static int normal, blind;
     static char[][] map;
     static boolean[][] visited;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
         N = Integer.parseInt(br.readLine());
         map = new char[N][N];
         visited = new boolean[N][N];
-        normal = 0;
-        blind = 0;
 
         for (int i = 0; i < N; i++) {
-            map[i] = br.readLine().toCharArray();
+            String str = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = str.charAt(j);
+            }
         }
 
-        for (int i = 0; i < N; i++) { // 비적록색약
+        int normal = 0;
+        for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
-                    BFS(i, j);
+                    dfs(i, j);
                     normal++;
                 }
             }
         }
 
+        int blind = 0;
         visited = new boolean[N][N];
-        for (int i = 0; i < N; i++) { // 적록색약
+        for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] == 'G') {
-                    map[i][j] = 'R';
-                }
+                if (map[i][j] == 'R') map[i][j] = 'G';
             }
         }
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
-                    BFS(i,j);
+                    dfs(i, j);
                     blind++;
                 }
             }
         }
-        System.out.println(normal + " " + blind);
+        sb.append(normal).append(" ").append(blind);
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
-    private static void BFS (int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y});
+
+    private static void dfs(int x, int y) {
         visited[x][y] = true;
-        char color = map[x][y];
+        char cur = map[x][y];
 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int cx = cur[0];
-            int cy = cur[1];
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-            for (int i = 0; i < 4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
-                    continue;
-                }
-                if (!visited[nx][ny] && map[nx][ny] == color) {
-                    visited[nx][ny] = true;
-                    q.add(new int[]{nx, ny});
-                }
-            }
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+            if (!visited[nx][ny] && map[nx][ny] == cur) dfs(nx, ny);
         }
     }
 }
