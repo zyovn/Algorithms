8e999a342f51cf59005d45501fbb42fb9fbffa27
Jeongbin Joo <yui2507@naver.com>
2026-03-15 16:27:28 +0900

    [Silver II] Title: 베르트랑 공준, Time: 244 ms, Memory: 29000 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main {
     static int n;
-    static boolean[] isPrime;
+    static boolean[] isComposite;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -10,21 +10,21 @@ public class Main {
 
         while (true) {
             n = Integer.parseInt(br.readLine());
-            isPrime = new boolean[n * 2 + 1];
+            isComposite = new boolean[n * 2 + 1];
 
             if (n == 0) break;
 
             for (int i = 2; i <= n * 2; i++) {
-                if (!isPrime[i]) {
+                if (!isComposite[i]) {
                     for (int j = i * 2; j <= n * 2; j += i) {
-                        if (!isPrime[j]) isPrime[j] = true;
+                        isComposite[j] = true;
                     }
                 }
             }
 
             int cnt = 0;
             for (int i = n + 1; i <= n * 2; i++) {
-                if (!isPrime[i]) cnt++;
+                if (!isComposite[i]) cnt++;
             }
             sb.append(cnt).append("\n");
         }
