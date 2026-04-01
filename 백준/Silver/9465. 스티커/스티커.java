9cb02a46e79aa8e37c75f8d2566b4569c39b536f
Jeongbin Joo <yui2507@naver.com>
2026-04-01 16:28:06 +0900

    [Silver I] Title: 스티커, Time: 732 ms, Memory: 113892 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int T, n;
+    static int[][] sticker, dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            n = Integer.parseInt(br.readLine());
+            sticker = new int[2][n + 1];
+            dp = new int[2][n + 1];
+
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= n; j++) {
+                sticker[0][j] = Integer.parseInt(st1.nextToken());
+                sticker[1][j] = Integer.parseInt(st2.nextToken());
+            }
+            dp[0][1] = sticker[0][1];
+            dp[1][1] = sticker[1][1];
+
+            for (int j = 2; j <= n; j++) {
+                dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + sticker[0][j];
+                dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + sticker[1][j];
+            }
+            sb.append(Math.max(dp[0][n], dp[1][n])).append("\n");
+        }
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
