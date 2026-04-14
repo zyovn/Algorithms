7de967484cdd9d11c7b81e7a9f86126eb7d48126
Jeongbin Joo <yui2507@naver.com>
2026-04-14 11:21:15 +0900

    [level 2] Title: 전화번호 목록, Time: 343.89 ms, Memory: 99.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,17 +2,10 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        HashMap<String, Integer> map = new HashMap<>();
-        int len = phone_book.length;
+        Arrays.sort(phone_book);
         
-        for (int i = 0; i < len; i++) {
-            map.put(phone_book[i], map.getOrDefault(phone_book[i], 0) + 1);
-        }
-        
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < phone_book[i].length(); j++) {
-                if (map.containsKey(phone_book[i].substring(0, j))) return false;
-            }
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
         }
         return true;
     }
